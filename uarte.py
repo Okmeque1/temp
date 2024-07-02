724bf48fbe35f9d3d0bd8125814238497340cfa3
Okmeque1 <121556749+Okmeque1@users.noreply.github.com>
2024-07-02 17:30:50 +0100

    Update uarte.py

--- uarte.py
+++ uarte.py
@@ -3,5 +3,10 @@ def generate():
     password = ''
     for x in range(3):
       password += random.choice('!"$£%$^&%$£T$(IEGDDVOG)E')
-    print(password)
+    print(password)#if you do this for some reason CodeQL is pissed because the variable is called 'password', even though this is not a password generator
+    
 generate()
+
+
+password = 'hello'
+print(password)#test number 2
